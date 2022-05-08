c5e0d9e3f58e511d883a981af6074b6a6dde9687
Balu Prem Chand <68683520+premchand-9@users.noreply.github.com>
2022-05-08 11:08:40 +0530

    Delete src/Components/user directory

--- src/Components/user/Allrequests.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Allrequests() {
-  return (
-    <div>Allrequests</div>
-  )
-}
--- src/Components/user/Myrequests.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function Myrequests() {
-  return (
-    <div>Myrequests</div>
-  )
-}
